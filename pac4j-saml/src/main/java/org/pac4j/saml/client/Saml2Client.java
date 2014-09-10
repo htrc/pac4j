98be7d626da7511e7a969cae428c825463556bb2
Samitha Liyanage <samitha.liyanage@gmail.com>
2014-09-09 20:24:17 -0400

    fix for trust manager

--- pac4j-saml/src/main/java/org/pac4j/saml/client/Saml2Client.java
+++ pac4j-saml/src/main/java/org/pac4j/saml/client/Saml2Client.java
@@ -464,7 +464,7 @@ public class Saml2Client extends BaseClient<Saml2Credentials, Saml2Profile> {
         TrustManager[] trustAllCerts = new TrustManager[] {
                 new X509TrustManager() {
                     public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
+                        return null;
                     }
                     public void checkClientTrusted(
                             java.security.cert.X509Certificate[] certs, String authType) {
